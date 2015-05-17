e37a85258f72c88d86f46982ec50705174e2ad5c
Forsazh <Forsazh.rus@mail.ru>
2015-05-17 23:37:35 +0300

    Start working

--- src/main/java/com/kfu/training/parser/CrosswordParserManually.java
+++ src/main/java/com/kfu/training/parser/CrosswordParserManually.java
@@ -1,7 +1,12 @@
 package com.kfu.training.parser;
 
+import java.lang.System;
+
 /**
  * Created by RuzilyaS on 17-May-15.
  */
 public class CrosswordParserManually {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
 }
